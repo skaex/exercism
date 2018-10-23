1af5a7d849085b34a797861672d5f4e92bf47259
SkaeX <abdulmajid.hamza@aun.edu.ng>
2018-10-23 14:50:15 +0100

    TypeScript: Hello World

---
+++ typescript/hello-world/hello-world.ts
@@ -0,0 +1,10 @@
+/**
+ * Hello to the World of Typescript on Exercism
+ */
+class HelloWorld {
+  static hello(noun: string = 'World') {
+    return `Hello, ${noun}!`
+  }
+}
+
+export default HelloWorld
