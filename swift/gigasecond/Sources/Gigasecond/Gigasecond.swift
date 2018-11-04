3787013d672c875ca07b790b89ce7e82d8fce89b
skaex <abdulmajid.hamza@aun.edu.ng>
2018-11-05 00:25:38 +0100

    Swift: GigaSecond

---
+++ swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+struct Gigasecond {
+   
+   var description: String = ""
+   
+   init?(from: String) {
+      let dateFormatter = DateFormatter()
+      dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+      dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+      dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+      
+      
+      if let startDate = dateFormatter.date(from: from) {
+         print(startDate)
+         self.description = dateFormatter.string(
+            from: Date(timeInterval: 1_000_000_000, since: startDate)
+         )
+      }
+   }
+}
