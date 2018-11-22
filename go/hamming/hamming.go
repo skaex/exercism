4d9b87181ca7766043b2714b0a5281ef87d86152
skaex <abdulmajid.hamza@aun.edu.ng>
2018-11-22 21:56:11 +0100

    Go: Hamming

---
+++ go/hamming/hamming.go
@@ -0,0 +1,20 @@
+// Package hamming - Exercism -> Go -> Hamming.
+package hamming
+
+import "errors"
+
+// Distance calculates hamming distance between nucleotide a and b.
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("nucleotide size must be the same")
+	}
+
+	distance := 0
+	for index := 0; index < len(a); index++ {
+		if a[index] != b[index] {
+			distance++
+		}
+	}
+
+	return distance, nil
+}
