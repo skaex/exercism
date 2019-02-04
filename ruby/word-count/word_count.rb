8d7cb99ba31147fa5190c8326b7199bfdfefc1ec
skaex <abdulmajid.hamza@aun.edu.ng>
2019-02-04 19:55:50 +0100

    Ruby: Update Word Count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,12 +1,15 @@
 # All the phrases and all
 class Phrase
-  attr_reader :word_count
+  attr_reader :phrase
 
   def initialize(phrase)
-    @word_count =
-      phrase.strip.split(/[\s,]+/).each_with_object({}) do |word, object|
-        key = word.gsub(/\A[\W]+|[\W]+\z/, '').downcase
-        object[key] = object[key].to_i + 1
-      end
+    @phrase = phrase
+  end
+
+  def word_count
+    phrase.strip.split(/[\s,]+/).each_with_object({}) do |word, object|
+      key = word.gsub(/\A[\W]+|[\W]+\z/, '').downcase
+      object[key] = object[key].to_i + 1
+    end
   end
 end
