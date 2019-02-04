419e7f4c5f3018ca43b49279047194993a9a3f94
skaex <abdulmajid.hamza@aun.edu.ng>
2019-02-04 20:11:37 +0100

    Go: Update scrabble score

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -5,27 +5,20 @@ import "strings"
 
 // Score calculates the score of word using some mapping rules from
 // exercism.io
-// It returns the score of the word as an int
+var sm = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1,
+	'S': 1, 'T': 1, 'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
+}
+
+// Score returns the score of the word as an int
 func Score(word string) int {
-	sm := map[string]int{
-		"AEIOULNRST": 1,
-		"DG":         2,
-		"BCMP":       3,
-		"FHVWY":      4,
-		"K":          5,
-		"JX":         8,
-		"QZ":         10,
+	score := 0
+
+	for _, chr := range strings.ToUpper(word) {
+		score += sm[chr]
 	}
 
-	sc := 0
-
-	for _, chr := range word {
-		for k, v := range sm {
-			if strings.Contains(k, strings.ToUpper(string(chr))) {
-				sc += v
-			}
-		}
-	}
-
-	return sc
+	return score
 }
