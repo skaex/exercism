373131c1e773bd6fa2ed512c65c7a739d8cfec82
skaex <abdulmajid.hamza@aun.edu.ng>
2019-02-13 16:25:05 +0100

    Go: Update isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -7,15 +7,16 @@ import (
 
 // IsIsogram returns true if the phrase passed is an isogram and false if otherwise
 func IsIsogram(phrase string) bool {
-	tracker := map[rune]int{}
-	for _, word := range strings.ToUpper(phrase) {
-		if unicode.IsLetter(word) {
-			tracker[word]++
-		}
-
-		if tracker[word] > 1 {
+	seen := map[rune]bool{}
+	for _, r := range strings.ToUpper(phrase) {
+		if seen[r] {
 			return false
 		}
+
+		if unicode.IsLetter(r) {
+			seen[r] = true
+		}
+
 	}
 	return true
 }
