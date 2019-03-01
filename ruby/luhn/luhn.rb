bd102988c40d4ad155ccf987ac02bd0f7fb6aff1
skaex <abdulmajid.hamza@aun.edu.ng>
2019-03-01 12:43:49 +0100

    Ruby: Update luhn

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -12,14 +12,14 @@ class Luhn
   end
 
   def valid?
-    digits_length_ok? &&
-      digits_luhn_valid? &&
-      only_digits?
+    two_or_more_digits? &&
+      only_digits? &&
+      (luhn_sum % 10).zero?
   end
 
   private
 
-  def digits_length_ok?
+  def two_or_more_digits?
     digits.size > 1
   end
 
@@ -27,21 +27,20 @@ class Luhn
     input_wos.size.eql?(digits.size)
   end
 
-  def digits_luhn_valid?
-    (digits_luhn_sum % 10).zero?
-  end
-
-  def digits_luhn_sum
+  def luhn_sum
     digits
       .reverse
       .each_with_index
-      .map { |digit, index| calculate_luhn_double(index + 1, digit.to_i) }
-      .sum
+      .sum { |digit, index| luhn_double(index + 1, digit.to_i) }
   end
 
-  def calculate_luhn_double(index, digit)
-    return ((digit * 2) > 9 ? digit * 2 - 9 : digit * 2) if (index % 2).zero?
+  def luhn_double(index, digit)
+    if index.even?
+      double = digit * 2
 
-    digit
+      double > 9 ? double - 9 : double
+    else
+      digit
+    end
   end
 end
