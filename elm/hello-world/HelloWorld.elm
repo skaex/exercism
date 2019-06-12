d96cf853bffb0673a10f5792d0421074beb56cad
skaex <abdulmajid.hamza@gmail.com>
2019-06-12 18:39:31 +0100

    Elm: Hello World

---
+++ elm/hello-world/HelloWorld.elm
@@ -0,0 +1,15 @@
+{-
+   This is a "stub" file.  It's a little start on your solution. It's not a
+   complete solution though; you have to write some code.
+
+   The module name is expected by the test program and must match the name of this
+   file. It has to stay just the way it is.
+-}
+
+
+module HelloWorld exposing (helloWorld)
+
+
+helloWorld : String
+helloWorld =
+   "Hello, World!"
