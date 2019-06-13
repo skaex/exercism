62809fb8ee2e8b91b94d257f25eda5273dffa6cc
skaex <abdulmajid.hamza@gmail.com>
2019-06-13 11:55:13 +0100

    Go: Update Luhn

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,35 +1,32 @@
 // Package luhn implements the luhn exercise on exercism.io
 package luhn
 
-import (
-	"regexp"
-	"strconv"
-	"strings"
-)
+import "strings"
 
 // Valid determines if input string is a valid luhn sequence
 func Valid(input string) bool {
 	input = strings.ReplaceAll(input, " ", "")
 
-	nonDigitRegex := regexp.MustCompile(`\D`)
-
 	size := len(input)
-	if nonDigitRegex.MatchString(input) || size <= 1 {
+
+	if size <= 1 {
 		return false
 	}
 
-	s := strings.Split(input, "")
 	sum := 0
-	for idx := range s {
-		num, _ := strconv.Atoi(string(s[size-idx-1]))
+
+	for idx := range input {
+		num := int(input[size-idx-1] - '0')
+		if num < 0 || num > 9 {
+			return false
+		}
 
 		if idx%2 == 1 {
 			doubled := num * 2
 			if doubled > 9 {
-				sum += doubled - 9
-			} else {
-				sum += doubled
+				doubled -= 9
 			}
+			sum += doubled
 		} else {
 			sum += num
 		}
