47711901ccea64aee16eab523857a81b518266dd
skaex <abdulmajid.hamza@gmail.com>
2019-06-22 08:26:54 +0100

    Go: Update Grains

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -5,7 +5,10 @@ import "errors"
 
 // Total calculate total number of grains on the chessboard
 func Total() uint64 {
-	total, _ := totalAndValueAtSquare(64)
+	total := uint64(1)
+	for i := 1; i < 64; i++ {
+		total += 1 << uint64(i)
+	}
 
 	return total
 }
@@ -15,19 +18,6 @@ func Square(position int) (uint64, error) {
 	if position < 1 || position > 64 {
 		return 0, errors.New("Square position invalid")
 	}
-	_, value := totalAndValueAtSquare(position)
 
-	return value, nil
-}
-
-// totalAndValueAtSquare helps calculate total and value at a square on a chessboard
-func totalAndValueAtSquare(position int) (uint64, uint64) {
-	value := uint64(1)
-	total := uint64(1)
-	for i := 1; i < position; i++ {
-		value *= 2
-		total += value
-	}
-
-	return total, value
+	return 1 << uint64(position-1), nil
 }
