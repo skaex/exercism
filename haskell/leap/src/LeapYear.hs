24b62cd75a361966c2c760a5b4c1f1bef974379b
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2019-10-24 22:40:59 +0100

    Haskell: Update Leap

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -2,10 +2,6 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year =
-  isEvenlyDivisibleBy year 4 &&
-    (not (isEvenlyDivisibleBy year 100) ||
-      isEvenlyDivisibleBy year 400)
-
-isEvenlyDivisibleBy :: Integer -> Integer -> Bool
-isEvenlyDivisibleBy number divisor =
-  number `mod` divisor == 0
+  year `rem` 4 == 0 &&
+    (year `rem` 100 /= 0 ||
+      year `rem` 400 == 0)
