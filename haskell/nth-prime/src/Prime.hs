2ad9cd65d91b0fefefe4a4f309aac7487f23edb3
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2019-10-27 22:40:35 +0100

    Haskell: Nth Prime

---
+++ haskell/nth-prime/src/Prime.hs
@@ -0,0 +1,11 @@
+module Prime (nth) where
+
+nth :: Int -> Maybe Integer
+nth n
+  | n < 1 = Nothing
+  | otherwise = Just (last (take n primes))
+
+primes :: [Integer]
+primes = filterPrime [2..]
+  where filterPrime [] = []
+        filterPrime (p:xs) = p : filterPrime [ x | x <- xs, x `mod` p /= 0 ]
