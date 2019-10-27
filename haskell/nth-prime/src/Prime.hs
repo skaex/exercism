82a1fb2967af7a1e857f15fe643dcaf8bfad77de
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2019-10-27 22:43:31 +0100

    Haskell: Update Nth Prime

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -3,7 +3,7 @@ module Prime (nth) where
 nth :: Int -> Maybe Integer
 nth n
   | n < 1 = Nothing
-  | otherwise = Just (last (take n primes))
+  | otherwise = Just $ last $ take n primes
 
 primes :: [Integer]
 primes = filterPrime [2..]
