f56c7b6437e853cbe38b0cbbd3c60df2b017b5d0
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2019-10-27 23:02:55 +0100

    Haskell: Update SpaceAge

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -10,15 +10,14 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds =
-  let earthSeconds = 365.25 * 24 * 60 * 60
-  in case planet of
-    Mercury   -> seconds / (0.2408467 * earthSeconds)
-    Venus     -> seconds / (0.61519726 * earthSeconds)
-    Earth     -> seconds / (1.0 * earthSeconds)
-    Mars      -> seconds / (1.8808158 * earthSeconds)
-    Jupiter   -> seconds / (11.862615 * earthSeconds)
-    Saturn    -> seconds / (29.447498 * earthSeconds)
-    Uranus    -> seconds / (84.016846 * earthSeconds)
-    Neptune   -> seconds / (164.79132 * earthSeconds)
+ageOn planet = (/ (ratio * 365.25 * 24 * 60 * 60))
+    where ratio = case planet of
+                    Mercury   -> 0.2408467
+                    Venus     -> 0.61519726
+                    Earth     -> 1.0
+                    Mars      -> 1.8808158
+                    Jupiter   -> 11.862615
+                    Saturn    -> 29.447498
+                    Uranus    -> 84.016846
+                    Neptune   -> 164.79132
 
