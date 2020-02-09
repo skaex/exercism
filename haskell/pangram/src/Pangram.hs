3cc2d50d0dca015cbf4c5954d1609f9385cb2988
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-02-09 20:24:08 +0100

    Haskell: Update pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,7 +1,8 @@
-module Pangram (isPangram) where
+module Pangram
+  ( isPangram
+  ) where
+
 import Data.Char
-import Data.List
 
 isPangram :: String -> Bool
-isPangram text = trimmed == ['a'..'z']
-  where trimmed = nub . sort . map toLower . filter isAlpha . filter isAscii $ text 
+isPangram text = all (`elem` [toLower ch | ch <- text]) ['a' .. 'z']
