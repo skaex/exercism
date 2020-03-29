481c3452d76fa988695c98a99179cb11b4a82f54
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-03-29 19:01:53 +0100

    Haskell: Update Collatz

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -7,14 +7,11 @@ nextCollatz n
   | even n = div n 2
   | otherwise = 3 * n + 1
 
+collatzCount :: Integer -> Integer -> Integer
+collatzCount 1 c = c
+collatzCount n c = collatzCount (nextCollatz n) (c + 1)
+
 collatz :: Integer -> Maybe Integer
 collatz n
   | n < 1 = Nothing
-  | n == 1 = Just 0
-  | nc == 1 = Just 1
-  | otherwise =
-    case collatz nc of
-      Just c  -> Just (1 + c)
-      Nothing -> Nothing
-  where
-    nc = nextCollatz n
+  | otherwise = Just (collatzCount n 0)
