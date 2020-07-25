43e933c97a46d147b3b95f83a3ecea8b8ad217b3
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-07-25 12:43:30 +0100

    Ruby: Update Resistor Color Duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -3,41 +3,22 @@
 # Resistor Color Duo exercise from exercism.io
 # ResistorColorDuo class calculates value of a resistor
 class ResistorColorDuo
-  VALUES = {
-    'black' => '0',
-    'brown' => '1',
-    'red' => '2',
-    'orange' => '3',
-    'yellow' => '4',
-    'green' => '5',
-    'blue' => '6',
-    'violet' => '7',
-    'grey' => '8',
-    'white' => '9'
-  }.freeze
+  VALUES = %w[
+    black
+    brown
+    red
+    orange
+    yellow
+    green
+    blue
+    violet
+    grey
+    white
+  ].freeze
 
   def self.value(bands)
     first, second, = bands
 
-    new(first, second).value
-  end
-
-  def initialize(first, second)
-    @first = first
-    @second = second
-  end
-
-  def value
-    total_value = band_value(first) + band_value(second)
-
-    total_value.to_i
-  end
-
-  private
-
-  attr_reader :first, :second
-
-  def band_value(band)
-    VALUES.fetch(band, '')
+    "#{VALUES.index(first)}#{VALUES.index(second)}".to_i
   end
 end
