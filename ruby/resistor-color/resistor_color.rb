949693228c93aaf457777751fcf0b85699bcd08b
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-07-25 12:35:40 +0100

    Ruby: Update Resistor Color

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -2,21 +2,20 @@
 
 # The ResistorColor class represent resistors with color bands
 class ResistorColor
-  MAPPING = {
-    'black' => 0,
-    'brown' => 1,
-    'red' => 2,
-    'orange' => 3,
-    'yellow' => 4,
-    'green' => 5,
-    'blue' => 6,
-    'violet' => 7,
-    'grey' => 8,
-    'white' => 9
-  }.freeze
-  COLORS = MAPPING.keys
+  COLORS = %w[
+    black
+    brown
+    red
+    orange
+    yellow
+    green
+    blue
+    violet
+    grey
+    white
+  ].freeze
 
   def self.color_code(color)
-    MAPPING[color]
+    COLORS.index(color)
   end
 end
