ee38f62ad14854da4cc8d97ce41e83449705e429
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-07-25 11:06:13 +0100

    Elm: Hamming

---
+++ elm/hamming/src/Hamming.elm
@@ -0,0 +1,21 @@
+module Hamming exposing (distance)
+
+
+distance : String -> String -> Result String Int
+distance left right =
+    if String.length left /= String.length right then
+        Err "left and right strands must be of equal length"
+
+    else
+        List.map2
+            (\a b ->
+                if a == b then
+                    0
+
+                else
+                    1
+            )
+            (String.toList left)
+            (String.toList right)
+            |> List.sum
+            |> Ok
