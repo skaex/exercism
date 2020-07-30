44d8d0c7f03de754450a2af66df899d8bb590885
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-07-30 17:11:00 +0100

    Elm: Update Accumulate

--- elm/accumulate/src/Accumulate.elm
+++ elm/accumulate/src/Accumulate.elm
@@ -3,9 +3,14 @@ module Accumulate exposing (accumulate)
 
 accumulate : (a -> b) -> List a -> List b
 accumulate func input =
+    accumulateHelper func [] input
+
+
+accumulateHelper : (a -> b) -> List b -> List a -> List b
+accumulateHelper func result input =
     case input of
         [] ->
-            []
+            List.reverse result
 
         head :: tail ->
-            func head :: accumulate func tail
+            accumulateHelper func (func head :: result) tail
