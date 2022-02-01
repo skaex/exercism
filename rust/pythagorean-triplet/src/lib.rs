e8d1dbf3b017a0fe60306c95c6a6d6da2e0126e1
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2022-02-01 23:34:05 +0100

    Rust: Add pythagorean triplet

---
+++ rust/pythagorean-triplet/src/lib.rs
@@ -0,0 +1,14 @@
+use std::collections::HashSet;
+
+pub fn find(sum: u32) -> HashSet<[u32; 3]> {
+    let mut result = HashSet::new();
+    for a in 1..(sum / 3) {
+        for b in a..(sum / 2) {
+            let c = sum - a - b;
+            if a * a + b * b == c * c {
+                result.insert([a, b, c]);
+            }
+        }
+    }
+    result
+}
